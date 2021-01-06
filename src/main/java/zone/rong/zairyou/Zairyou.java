055ffc845988c541cb83ba0a0582aaa5b82aa0fb
Rongmario <ethanmahmood@gmail.com>
2021-01-06 15:40:36 +0000

    Change name to something less conflicting

--- src/main/java/zone/rong/zairyou/Zairyou.java
+++ src/main/java/zone/rong/zairyou/Zairyou.java
@@ -1,20 +1,17 @@
 package zone.rong.zairyou;
 
-import net.minecraft.potion.PotionType;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.registry.ForgeRegistries;
 import net.minecraftforge.oredict.OreDictionary;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import zone.rong.zairyou.api.creativetab.PotionCreativeTab;
 import zone.rong.zairyou.api.item.MaterialItem;
 import zone.rong.zairyou.api.material.Material;
-import zone.rong.zairyou.objects.Items;
+import zone.rong.zairyou.objects.ZairyouItems;
 import zone.rong.zairyou.objects.Materials;
 
 @Mod(modid = Zairyou.ID, name = Zairyou.NAME, version = "0.1")
@@ -35,7 +32,7 @@ public class Zairyou {
     public void preInit(FMLPreInitializationEvent event) {
         MinecraftForge.EVENT_BUS.register(this);
         Materials.init();
-        Items.init();
+        ZairyouItems.init();
     }
 
     @Mod.EventHandler
@@ -43,7 +40,7 @@ public class Zairyou {
 
     @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent event) {
-        Items.oreDictInit();
+        ZairyouItems.oreDictInit();
         Material.REGISTRY.values().stream()
                 .map(Material::getItems)
                 .flatMap(m -> m.values().stream())
--- src/main/java/zone/rong/zairyou/objects/ZairyouItems.java
+++ src/main/java/zone/rong/zairyou/objects/ZairyouItems.java
@@ -3,7 +3,7 @@ package zone.rong.zairyou.objects;
 import net.minecraftforge.oredict.OreDictionary;
 import zone.rong.zairyou.api.item.BasicItem;
 
-public class Items {
+public class ZairyouItems {
 
     public static final BasicItem GLOB_ROSIN = BasicItem.of("thermalfoundation", "rosin_glob", "glob_rosin");
     public static final BasicItem GLOB_TAR = BasicItem.of("thermalfoundation", "tar_glob", "glob_tar");
